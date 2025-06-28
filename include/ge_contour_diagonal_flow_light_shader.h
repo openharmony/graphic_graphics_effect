a8bf5acf3a171939612138baaf8de57705a26d27
ZIQIWANG <wangziqi9@huawei.com>
2025-06-28 23:32:41 +0800

    异形路径流光效果 Signed-off-by: wangziqi<wangziqi9@huawei.com>

---
+++ include/ge_contour_diagonal_flow_light_shader.h
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef GRAPHICS_EFFECT_CONTOUR_DIAGONAL_FLOW_LIGHT_SHADER_H
+#define GRAPHICS_EFFECT_CONTOUR_DIAGONAL_FLOW_LIGHT_SHADER_H
+
+#include "ge_shader.h"
+#include "common/rs_vector4.h"
+#include "effect/runtime_shader_builder.h"
+#include "utils/matrix.h"
+#include "ge_shader_filter_params.h"
+
+namespace OHOS {
+namespace Rosen {
+
+class GE_EXPORT GEContourDiagonalFlowLightShader : public GEShader {
+public:
+    GEContourDiagonalFlowLightShader();
+    GEContourDiagonalFlowLightShader(Drawing::GEContentDiagonalFlowLightShaderParams& contourDiagonalFlowLightParams);
+
+    ~GEContourDiagonalFlowLightShader() override = default;
+
+    void MakeDrawingShader(const Drawing::Rect& rect, float progress) override;
+
+    const std::string GetDescription() const { return "GEContourDiagonalFlowLightShader"; }
+
+    void SetGEContentDiagonalFlowLightShaderParams(const Drawing::GEContentDiagonalFlowLightShaderParams& params)
+    {
+        contourDiagonalFlowLightParams_ = params;
+    }
+
+    void Preprocess(Drawing::Canvas& canvas, const Drawing::Rect& rect) override;
+
+    std::shared_ptr<Drawing::ShaderEffect> MakeContourDiagonalFlowLightShader(const Drawing::Rect& rect);
+
+    static std::shared_ptr<GEContourDiagonalFlowLightShader>
+        CreateContourDiagonalFlowLightShader(Drawing::GEContentDiagonalFlowLightShaderParams& param);
+
+    std::shared_ptr<Drawing::RuntimeShaderBuilder> GetContourDiagonalFlowLightBuilder();
+
+    std::shared_ptr<Drawing::Image> MakeContourDiagonalFlowLightPrecalculationShader(Drawing::Canvas& canvas,
+        const Drawing::ImageInfo& imageInfo);
+
+    std::shared_ptr<Drawing::RuntimeShaderBuilder> GetContourDiagonalFlowLightPrecalculationBuilder();
+private:
+    GEContourDiagonalFlowLightShader(const GEContourDiagonalFlowLightShader&) = delete;
+    GEContourDiagonalFlowLightShader(const GEContourDiagonalFlowLightShader&&) = delete;
+    GEContourDiagonalFlowLightShader& operator=(const GEContourDiagonalFlowLightShader&) = delete;
+    GEContourDiagonalFlowLightShader& operator=(const GEContourDiagonalFlowLightShader&&) = delete;
+    static std::vector<float> PathStringToFloats(const std::string& str);
+    Drawing::GEContentDiagonalFlowLightShaderParams contourDiagonalFlowLightParams_;
+    std::shared_ptr<Drawing::RuntimeShaderBuilder> builder_;
+    std::vector<float> controlPoints_;
+};
+
+} // namespace Rosen
+} // namespace OHOS
+#endif // GRAPHICS_EFFECT_EXT_DOT_MATRIX_SHADER_H
---
+++ test/unittest/ge_contour_diagonal_flow_light_shader_test.cpp
@@ -0,0 +1,158 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+#include "ge_contour_diagonal_flow_light_shader.h"
+#include "ge_log.h"
+#include "draw/canvas.h"
+#include "image/bitmap.h"
+#include "utils/rect.h"
+#include "draw/color.h"
+#include "draw/path.h"
+#include "ge_shader_filter_params.h"
+
+using namespace testing;
+using namespace testing::ext;
+using namespace OHOS::Rosen;
+using namespace Drawing;
+
+namespace OHOS {
+namespace Rosen {
+
+class GEContourDiagonalFlowLightShaderTest : public Test {
+public:
+    static void SetUpTestCase()
+    {}
+    static void TearDownTestCase()
+    {}
+    void SetUp() override
+    {}
+    void TearDown() override
+    {}
+
+    static inline Canvas canvas_;
+};
+
+HWTEST_F(GEContourDiagonalFlowLightShaderTest, CreateContourDiagonalFlowLightShader_001, TestSize.Level1)
+{
+    // Test case for static CreateContourDiagonalFlowLightShader method
+    GEContentDiagonalFlowLightShaderParams params;
+    params.line1Start_ = 0.0f;
+    params.line1Length_ = 0.3f;
+    params.line1Color_ = Vector4f(0.1f, 0.2f, 0.3f, 0.4f);
+    params.line2Start_ = 0.5f;
+    params.line2Length_ = 0.3f;
+    params.line2Color_ = Vector4f(0.5f, 0.6f, 0.7f, 0.8f);
+    params.thickness_ = 0.02f;
+
+    auto shader = GEContourDiagonalFlowLightShader::CreateContourDiagonalFlowLightShader(params);
+    EXPECT_NE(shader, nullptr);
+}
+
+HWTEST_F(GEContourDiagonalFlowLightShaderTest, GEContourDiagonalFlowLightShaderConstructor_001, TestSize.Level1)
+{
+    // Test case for constructor with parameters
+    GEContentDiagonalFlowLightShaderParams testParams;
+    testParams.line1Start_ = 0.2f;
+    testParams.line1Length_ = 0.3f;
+    testParams.line1Color_ = Vector4f(0.6f, 0.3f, 0.9f, 1.0f);
+    testParams.line2Start_ = 0.7f;
+    testParams.line2Length_ = 0.5f;
+    testParams.line2Color_ = Vector4f(0.5f, 0.6f, 0.7f, 0.8f);
+    testParams.thickness_ = 0.2f;
+
+    GEContourDiagonalFlowLightShader shader(testParams);
+    EXPECT_FLOAT_EQ(shader.contourDiagonalFlowLightParams_.line1Start_, testParams.line1Start_);
+    EXPECT_FLOAT_EQ(shader.contourDiagonalFlowLightParams_.line1Length_, testParams.line1Length_);
+    EXPECT_FLOAT_EQ(shader.contourDiagonalFlowLightParams_.line1Color_[0], testParams.line1Color_[0]);
+    EXPECT_FLOAT_EQ(shader.contourDiagonalFlowLightParams_.line1Color_[1], testParams.line1Color_[1]);
+    EXPECT_FLOAT_EQ(shader.contourDiagonalFlowLightParams_.line1Color_[2], testParams.line1Color_[2]);
+    EXPECT_FLOAT_EQ(shader.contourDiagonalFlowLightParams_.line1Color_[3], testParams.line1Color_[3]);
+    EXPECT_FLOAT_EQ(shader.contourDiagonalFlowLightParams_.line2Start_, testParams.line2Start_);
+    EXPECT_FLOAT_EQ(shader.contourDiagonalFlowLightParams_.line2Length_, testParams.line2Length_);
+    EXPECT_FLOAT_EQ(shader.contourDiagonalFlowLightParams_.line2Color_[0], testParams.line2Color_[0]);
+    EXPECT_FLOAT_EQ(shader.contourDiagonalFlowLightParams_.line2Color_[1], testParams.line2Color_[1]);
+    EXPECT_FLOAT_EQ(shader.contourDiagonalFlowLightParams_.line2Color_[2], testParams.line2Color_[2]);
+    EXPECT_FLOAT_EQ(shader.contourDiagonalFlowLightParams_.line2Color_[3], testParams.line2Color_[3]);
+    EXPECT_FLOAT_EQ(shader.contourDiagonalFlowLightParams_.thickness_, testParams.thickness_);
+}
+HWTEST_F(GEContourDiagonalFlowLightShaderTest, MakeDrawingShader_001, TestSize.Level1)
+{
+    // Test case for MakeDrawingShader method
+    Drawing::Rect rect(0, 0, 100, 100);
+    float progress = 0.5f;
+    GEContentDiagonalFlowLightShaderParams params;
+    params.line1Start_ = 0.5f;
+    params.line1Length_ = 0.6f;
+    params.line1Color_ = Vector4f(0.6f, 0.3f, 0.9f, 1.0f);
+    params.line2Start_ = 0.0f;
+    params.line2Length_ = 0.2f;
+    params.line2Color_ = Vector4f(0.67f, 0.24f, 0.35f, 0.8f);
+    params.thickness_ = 0.25f;
+    auto shader = GEContourDiagonalFlowLightShader::CreateContourDiagonalFlowLightShader(params);
+    shader->MakeDrawingShader(rect, progress);
+    EXPECT_NE(shader->GetDrawingShader(), nullptr);
+}
+
+HWTEST_F(GEContourDiagonalFlowLightShaderTest, Preprocess_001, TestSize.Level1)
+{
+    // Test case for Preprocess method
+    Drawing::Rect rect(0, 0, 100, 100);
+    GEContentDiagonalFlowLightShaderParams params;
+    params.line1Start_ = 0.1f;
+    params.line1Length_ = 0.6f;
+    params.line1Color_ = Vector4f(0.6f, 0.3f, 0.9f, 1.0f);
+    params.line2Start_ = 0.6f;
+    params.line2Length_ = 0.2f;
+    params.line2Color_ = Vector4f(0.67f, 0.24f, 0.9f, 0.8f);
+    params.thickness_ = 0.25f;
+    auto shader = GEContourDiagonalFlowLightShader::CreateContourDiagonalFlowLightShader(params);
+    shader->Preprocess(canvas_, rect);
+    auto cache = shader->GetCache();
+    EXPECT_NE(cache, nullptr);
+}
+
+HWTEST_F(GEContourDiagonalFlowLightShaderTest, MakeContourDiagonalFlowLightShader_001, TestSize.Level1)
+{
+    // Test case for MakeContourDiagonalFlowLightShader method
+    Drawing::Rect rect(0, 0, 100, 100);
+    GEContentDiagonalFlowLightShaderParams params;
+    params.line1Start_ = 0.05f;
+    params.line1Length_ = 0.6f;
+    params.line1Color_ = Vector4f(0.8f, 0.5f, 1.0f, 1.0f);
+    params.line2Start_ = 0.55f;
+    params.line2Length_ = 0.2f;
+    params.line2Color_ = Vector4f(0.2f, 0.6f, 1.0f, 0.8f);
+    params.thickness_ = 0.5f;
+    auto shader = GEContourDiagonalFlowLightShader::CreateContourDiagonalFlowLightShader(params);
+    auto effect = shader->MakeContourDiagonalFlowLightShader(rect);
+    EXPECT_NE(effect, nullptr);
+}
+
+HWTEST_F(GEContourDiagonalFlowLightShaderTest, GetContourDiagonalFlowLightBuilder_001, TestSize.Level1)
+{
+    // Test case for GetContourDiagonalFlowLightBuilder method
+    GEContentDiagonalFlowLightShaderParams params;
+    params.line1Start_ = 0.2f;
+    params.line1Length_ = 0.8f;
+    params.line1Color_ = Vector4f(0.0f, 1.0f, 1.0f, 1.0f);
+    params.line2Start_ = 0.7f;
+    params.line2Length_ = 0.3f;
+    params.line2Color_ = Vector4f(1.0f, 0.0f, 1.0f, 1.0f);
+    params.thickness_ = 0.4f;
+    auto shader = GEContourDiagonalFlowLightShader::CreateContourDiagonalFlowLightShader(params);
+    auto builder = shader->GetContourDiagonalFlowLightBuilder();
+    EXPECT_NE(builder, nullptr);
+}
