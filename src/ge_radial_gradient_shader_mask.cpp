8e5c9d1d580fd6a8acee7a2f212212b75e79e319
zhangjz <zhangjiazhe1@huawei.com>
2025-07-03 20:57:13 +0800

    fix radial gradient bug: change one radius will change both axes of the ellipse
    Signed-off-by: zhangjz <zhangjiazhe1@huawei.com>

--- src/ge_radial_gradient_shader_mask.cpp
+++ src/ge_radial_gradient_shader_mask.cpp
@@ -59,45 +59,6 @@ std::shared_ptr<Drawing::RuntimeShaderBuilder> GERadialGradientShaderMask::GetRa
         uniform half colors[sizeArray];
         uniform half positions[sizeArray];
 
-        float sdEllipse(vec2 p, vec2 ab)
-        {
-            p = abs(p);
-            if (p.x > p.y) {
-                p = p.yx;
-                ab = ab.yx;
-            }
-            float l = ab.y * ab.y - ab.x * ab.x;
-            float m = ab.x * p.x / l;
-            float m2 = m * m;
-            float n = ab.y * p.y / l;
-            float n2 = n * n;
-            float c = (m2 + n2 - 1.0) / 3.0;
-            float c3 = c * c * c;
-            float q = c3 + m2 * n2 * 2.0;
-            float d = c3 + m2 * n2;
-            float g = m + m * n2;
-            float co;
-            if (d < 0.0)
-            {
-                float h = acos(q / c3) / 3.0;
-                float s = cos(h);
-                float t = sin(h) * sqrt(3.0);
-                float rx = sqrt(-c * (s + t + 2.0) + m2);
-                float ry = sqrt(-c * (s - t + 2.0) + m2);
-                co = (ry + sign(l) * rx + abs(g) / (rx * ry) - m) / 2.0;
-            } else {
-                float h = 2.0 * m * n * sqrt(d);
-                float s = sign(q + h) * pow(abs(q + h), 1.0 / 3.0);
-                float u = sign(q - h) * pow(abs(q - h), 1.0 / 3.0);
-                float rx = -s - u - c * 4.0 + 2.0 * m2;
-                float ry = (s - u) * sqrt(3.0);
-                float rm = sqrt(rx * rx + ry * ry);
-                co = (ry / sqrt(rm - rx) + 2.0 * g / rm - m) / 2.0;
-            }
-            vec2 r = ab * vec2(co, sqrt(1.0 - co * co));
-            return length(r - p) * sign(r.y - p.y);
-        }
-
         float colorGradient(float colorA, float colorB, float startPos, float endPos, float threshold)
         {
             if (startPos <= threshold && threshold < endPos) {
@@ -108,10 +69,7 @@ std::shared_ptr<Drawing::RuntimeShaderBuilder> GERadialGradientShaderMask::GetRa
 
         float radialGradientMask(vec2 uv, vec2 centerPosition)
         {
-            float sdfValue = length(uv - centerPosition) / radiusX;
-            if (abs(radiusX - radiusY) > 0.0001) {
-                sdfValue = 1.0 - sdEllipse(uv - centerPosition, vec2(radiusX, radiusY)) / min(radiusX, radiusY);
-            }
+            float sdfValue = length(uv - centerPosition) / radiusY;
             sdfValue = clamp(sdfValue, 0.0, 1.0);
 
             float color = 0.0;
@@ -126,8 +84,8 @@ std::shared_ptr<Drawing::RuntimeShaderBuilder> GERadialGradientShaderMask::GetRa
             vec2 uv = fragCoord.xy / iResolution.xy;
             float screenRatio = iResolution.x / iResolution.y;
             vec2 centeredUVs = uv * 2.0 - 1.0;
-            centeredUVs.x *= screenRatio;
-            vec2 centerPosition = centerPos * 2- 1.0 ;
+            centeredUVs.x *= screenRatio * (radiusY / radiusX);
+            vec2 centerPosition = centerPos * 2 - 1.0 ;
             centerPosition.x *= screenRatio;
 
             float finalColor = radialGradientMask(centeredUVs, centerPosition);
@@ -175,45 +133,6 @@ std::shared_ptr<Drawing::RuntimeShaderBuilder> GERadialGradientShaderMask::GetRa
         uniform half colors[sizeArray];
         uniform half positions[sizeArray];
 
-        float sdEllipse(vec2 p, vec2 ab)
-        {
-            p = abs(p);
-            if (p.x > p.y) {
-                p = p.yx;
-                ab = ab.yx;
-            }
-            float l = ab.y * ab.y - ab.x * ab.x;
-            float m = ab.x * p.x / l;
-            float m2 = m * m;
-            float n = ab.y * p.y / l;
-            float n2 = n * n;
-            float c = (m2 + n2 - 1.0) / 3.0;
-            float c3 = c * c * c;
-            float q = c3 + m2 * n2 * 2.0;
-            float d = c3 + m2 * n2;
-            float g = m + m * n2;
-            float co;
-            if (d < 0.0)
-            {
-                float h = acos(q / c3) / 3.0;
-                float s = cos(h);
-                float t = sin(h) * sqrt(3.0);
-                float rx = sqrt(-c * (s + t + 2.0) + m2);
-                float ry = sqrt(-c * (s - t + 2.0) + m2);
-                co = (ry + sign(l) * rx + abs(g) / (rx * ry) - m) / 2.0;
-            } else {
-                float h = 2.0 * m * n * sqrt(d);
-                float s = sign(q + h) * pow(abs(q + h), 1.0 / 3.0);
-                float u = sign(q - h) * pow(abs(q - h), 1.0 / 3.0);
-                float rx = -s - u - c * 4.0 + 2.0 * m2;
-                float ry = (s - u) * sqrt(3.0);
-                float rm = sqrt(rx * rx + ry * ry);
-                co = (ry / sqrt(rm - rx) + 2.0 * g / rm - m) / 2.0;
-            }
-            vec2 r = ab * vec2(co, sqrt(1.0 - co * co));
-            return length(r - p) * sign(r.y - p.y);
-        }
-
         float colorGradient(float colorA, float colorB, float startPos, float endPos, float threshold)
         {
             if (startPos <= threshold && threshold < endPos) {
@@ -224,10 +143,7 @@ std::shared_ptr<Drawing::RuntimeShaderBuilder> GERadialGradientShaderMask::GetRa
 
         vec4 radialGradientMask(vec2 uv, vec2 centerPosition)
         {
-            float sdfValue = length(uv - centerPosition) / radiusX;
-            if (abs(radiusX - radiusY) > 0.0001) {
-                sdfValue = 1.0 - sdEllipse(uv - centerPosition, vec2(radiusX, radiusY)) / min(radiusX, radiusY);
-            }
+            float sdfValue = length(uv - centerPosition) / radiusY;
             sdfValue = clamp(sdfValue, 0.0, 1.0);
 
             float color = 0.0;
@@ -244,8 +160,8 @@ std::shared_ptr<Drawing::RuntimeShaderBuilder> GERadialGradientShaderMask::GetRa
             vec2 uv = fragCoord.xy / iResolution.xy;
             float screenRatio = iResolution.x / iResolution.y;
             vec2 centeredUVs = uv * 2.0 - 1.0;
-            centeredUVs.x *= screenRatio;
-            vec2 centerPosition = centerPos * 2- 1.0 ;
+            centeredUVs.x *= screenRatio * (radiusY / radiusX);
+            vec2 centerPosition = centerPos * 2 - 1.0 ;
             centerPosition.x *= screenRatio;
 
             vec4 finalColor = radialGradientMask(centeredUVs, centerPosition);
@@ -276,6 +192,10 @@ std::shared_ptr<ShaderEffect> GERadialGradientShaderMask::GenerateShaderEffect(f
     size_t positionSize = param_.positions_.size();
     // 0.01f is the min value
     if (colorSize <= 0 || colorSize > SIZE_ARRAY || colorSize != positionSize || width < 0.01f || height < 0.01f) {
+        return nullptr;
+    }
+    // if radius <= 0, no need to draw. 0.001f is the min value
+    if (param_.radiusX_ < 0.001f || param_.radiusY_ < 0.001f) {
         return nullptr;
     }
 
