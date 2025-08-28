c8b0b4f9f4ce4a22dce3e4a6436bb6c2d7409b19
lingzhengyang <lingzhengyang@h-partners.com>
2025-08-28 12:22:47 +0000

    update src/ext/gex_marshalling_helper.cpp.
    Signed-off-by: lingzhengyang <lingzhengyang@h-partners.com>

--- src/ext/gex_marshalling_helper.cpp
+++ src/ext/gex_marshalling_helper.cpp
@@ -23,6 +23,7 @@ namespace OHOS {
 namespace Rosen {
 
 constexpr uint32_t MARSHALLING_SIZE_MAX_LIMIT = 100;  // 100 max length
+constexpr uint32_t MARSHALLING_SIZE_ONE_THOUSAND = 1000;  // 1000 max length
 
 bool RotateEffectParams::Marshalling(Parcel& parcel)
 {
@@ -360,7 +361,7 @@ bool GEXComplexShaderParams::Unmarshalling(Parcel& parcel)
             return false;
         }
         uint32_t paramsSize = intVal;
-        if (paramsSize > 1000) {
+        if (paramsSize > MARSHALLING_SIZE_ONE_THOUSAND) {
             GE_LOGE("GEXComplexShaderParams::Unmarshalling invalid paramsSize!");
             return false;
         }
