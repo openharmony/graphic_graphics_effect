592ff7f642da2431783305a409f059d11d4d58bd
zelunwu <wuzelun@huawei.com>
2025-10-11 12:47:32 +0800

    update: 更新文件 ge_particle_circular_halo_shader.h 圆环流光效果shader参数定稿
    Signed-off-by: zelunwu <wuzelun@huawei.com>

--- include/ge_particle_circular_halo_shader.h
+++ include/ge_particle_circular_halo_shader.h
@@ -24,34 +24,29 @@
 
 namespace OHOS {
 namespace Rosen {
-REGISTER_GEFILTER_TYPEINFO(PARTICLE_CIRCULAR_HALO, GEParticleCircularHaloShader,
-                           Drawing::GEParticleCircularHaloShaderParams, Drawing::GE_SHADER_PARTICLE_CIRCULAR_HALO);
+
 class GE_EXPORT GEParticleCircularHaloShader : public GEShader {
 public:
     GEParticleCircularHaloShader();
-
     GEParticleCircularHaloShader(Drawing::GEParticleCircularHaloShaderParams& params);
-
     ~GEParticleCircularHaloShader() override = default;
-    DECLARE_GEFILTER_TYPEFUNC(GEParticleCircularHaloShader);
     void MakeDrawingShader(const Drawing::Rect& rect, float progress) override;
-
-    const std::string GetDescription() const
-    {
-        return "GEParticleCircularHaloShader";
-    }
+    const std::string GetDescription() const{return "GEParticleCircularHaloShader";}
 
 private:
+    void Preprocess(Drawing::Canvas& canvas, const Drawing::Rect& rect) override;
+
     static std::shared_ptr<GEParticleCircularHaloShader> CreateParticleCircularHaloShader(
         Drawing::GEParticleCircularHaloShaderParams& params);
 
     std::shared_ptr<Drawing::RuntimeShaderBuilder> GetGlowHaloBuilder();
+    std::shared_ptr<Drawing::RuntimeShaderBuilder> GetSingleParticleHaloBuilder();
     std::shared_ptr<Drawing::RuntimeShaderBuilder> GetParticleHaloBuilder();
     std::shared_ptr<Drawing::RuntimeShaderBuilder> GetParticleCircularHaloBuilder();
 
-    void Preprocess(Drawing::Canvas& canvas, const Drawing::Rect& rect) override;
-
     std::shared_ptr<Drawing::Image> MakeGlowHaloShader(Drawing::Canvas& canvas,
+        const Drawing::ImageInfo& imageInfo);
+    std::shared_ptr<Drawing::Image> MakeSingleParticleHaloShader(Drawing::Canvas& canvas,
         const Drawing::ImageInfo& imageInfo);
     std::shared_ptr<Drawing::Image> MakeParticleHaloShader(Drawing::Canvas& canvas,
         const Drawing::ImageInfo& imageInfo);
@@ -64,12 +59,12 @@ private:
 
     Drawing::GEParticleCircularHaloShaderParams particleCircularHaloParams_;
     std::shared_ptr<Drawing::RuntimeShaderBuilder> glowHaloBuilder_ = nullptr;
+    std::shared_ptr<Drawing::RuntimeShaderBuilder> singleParticleHaloBuilder_ = nullptr;
     std::shared_ptr<Drawing::RuntimeShaderBuilder> particleHaloBuilder_ = nullptr;
     std::shared_ptr<Drawing::RuntimeShaderBuilder> builder_ = nullptr;
-    std::shared_ptr<Drawing::Image> particleHaloImg_ = nullptr;
 
-    float lastCenterX_ = 0.0f;
-    float lastCenterY_ = 0.0f;
+    std::shared_ptr<Drawing::Image> particleHaloImg_ = nullptr;
+    std::shared_ptr<Drawing::Image> glowHaloImg_ = nullptr;
 };
 
 }  // namespace Rosen
