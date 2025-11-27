e0a20dd2078aa4cc2410b244603a861fe8b565c2
tangrui94 <tangrui52@h-partners.com>
2025-11-27 16:02:07 +0800

    update: 更新文件 ge_visual_effect_impl.cpp
    Signed-off-by: tangrui94 <tangrui52@h-partners.com>

--- src/ge_visual_effect_impl.cpp
+++ src/ge_visual_effect_impl.cpp
@@ -1,7 +1,4 @@
 /*
- * Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
- */
-/*
  * Copyright (c) 2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
