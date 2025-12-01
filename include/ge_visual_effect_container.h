04dece45c219d09734dbbf221d4c70298daca982
panjiaoMTI <panjiao5@huawei.com>
2025-12-01 20:18:24 +0800

    send flag to cacheManager of RSMaterialFilterDrawable Signed-off-by: panjiao<panjiao5@huawei.com>

--- include/ge_visual_effect_container.h
+++ include/ge_visual_effect_container.h
@@ -35,7 +35,8 @@ public:
 
     void UpdateCacheDataFrom(const std::shared_ptr<GEVisualEffectContainer>& ge);
 
-    void SetGeometry(const Drawing::Matrix& matrix, const Drawing::RectF& bound, float geoWidth, float geoHeight);
+    void SetGeometry(const Drawing::Matrix& matrix, const Drawing::RectF& bound, const Drawing::RectF& materialDst,
+        float geoWidth, float geoHeight);
 
     void SetDisplayHeadroom(float headroom);
 
