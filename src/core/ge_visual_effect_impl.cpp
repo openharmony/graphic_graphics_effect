1bc0652655c8bafefccd31b9f62f2c3a3100233b
zelunwu <wuzelun@huawei.com>
2026-04-01 14:09:25 +0800

    update: 更新文件 ge_visual_effect_impl.cpp
    Signed-off-by: zelunwu <wuzelun@huawei.com>

--- src/core/ge_visual_effect_impl.cpp
+++ src/core/ge_visual_effect_impl.cpp
@@ -2940,9 +2940,9 @@ void GEVisualEffectImpl::SetFrostedGlassParams(const std::string& tag, float par
         frostedGlassParams_->bgAlpha = std::clamp(param, 0.0f, 1.0f); // valid alpha range is between 0.0f and 1.0f
     }
 
-    constexpr float MAX_COLOR = 15.0f;
+    constexpr float maxColorLitmit = 15.0f;
     if (tag == GE_FILTER_FROSTED_GLASS_MAXCOLOR) {
-        frostedGlassParams_->maxColor = std::clamp(param, 0.0f, MAX_COLOR);
+        frostedGlassParams_->maxColor = std::clamp(param, 0.0f, maxColorLitmit);
     }
 }
 
