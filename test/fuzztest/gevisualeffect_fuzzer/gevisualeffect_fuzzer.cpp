d558fa0e902c84a7729ab74155887ad81c555217
Startidal001 <18795137338@163.com>
2026-06-03 18:25:40 +0800

    update: 更新文件 gevisualeffect_fuzzer.cpp
    Signed-off-by: Startidal001 <18795137338@163.com>

--- test/fuzztest/gevisualeffect_fuzzer/gevisualeffect_fuzzer.cpp
+++ test/fuzztest/gevisualeffect_fuzzer/gevisualeffect_fuzzer.cpp
@@ -12,17 +12,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 #include <cstring>
 #include <memory>
 #include "gevisualeffect_fuzzer.h"
 #include "ge_render.h"
 #include <fuzzer/FuzzedDataProvider.h>
- 
+
 namespace OHOS {
 namespace Rosen {
 namespace Drawing {
- 
+
 namespace {
 constexpr size_t STR_LEN = 32;
 constexpr size_t VEC_SIZE_MAX = 4;
@@ -42,61 +42,61 @@ enum TestCaseIndex {
     TEST_CASE_POINT_ARRAY_PARAM = 8
 };
 }
- 
+
 void GEVisualEffectFuzzTestIntParams(FuzzedDataProvider& fdp)
 {
     std::string name = fdp.ConsumeRandomLengthString(STR_LEN);
     DrawingPaintType type = static_cast<DrawingPaintType>(fdp.ConsumeIntegral<uint8_t>());
     auto geVisualEffect = std::make_shared<GEVisualEffect>(name, type);
- 
+
     std::string tag = fdp.ConsumeRandomLengthString(STR_LEN);
     int32_t param32 = fdp.ConsumeIntegral<int32_t>();
     geVisualEffect->SetParam(tag, param32);
- 
+
     int64_t param64 = fdp.ConsumeIntegral<int64_t>();
     geVisualEffect->SetParam(tag, param64);
- 
+
     uint32_t paramU32 = fdp.ConsumeIntegral<uint32_t>();
     geVisualEffect->SetParam(tag, paramU32);
- 
+
     geVisualEffect->GetName();
     geVisualEffect->GetImpl();
 }
- 
+
 void GEVisualEffectFuzzTestFloatParams(FuzzedDataProvider& fdp)
 {
     std::string name = fdp.ConsumeRandomLengthString(STR_LEN);
     DrawingPaintType type = static_cast<DrawingPaintType>(fdp.ConsumeIntegral<uint8_t>());
     auto geVisualEffect = std::make_shared<GEVisualEffect>(name, type);
- 
+
     std::string tag = fdp.ConsumeRandomLengthString(STR_LEN);
     float paramF = fdp.ConsumeFloatingPoint<float>();
     geVisualEffect->SetParam(tag, paramF);
- 
+
     double paramD = fdp.ConsumeFloatingPoint<double>();
     geVisualEffect->SetParam(tag, paramD);
 }
- 
+
 void GEVisualEffectFuzzTestStringBoolParams(FuzzedDataProvider& fdp)
 {
     std::string name = fdp.ConsumeRandomLengthString(STR_LEN);
     DrawingPaintType type = static_cast<DrawingPaintType>(fdp.ConsumeIntegral<uint8_t>());
     auto geVisualEffect = std::make_shared<GEVisualEffect>(name, type);
- 
+
     std::string tag = fdp.ConsumeRandomLengthString(STR_LEN);
     std::string paramStr = fdp.ConsumeRandomLengthString(STR_LEN);
     geVisualEffect->SetParam(tag, paramStr.c_str());
- 
+
     bool paramBool = fdp.ConsumeBool();
     geVisualEffect->SetParam(tag, paramBool);
 }
- 
+
 void GEVisualEffectFuzzTestMatrixParam(FuzzedDataProvider& fdp)
 {
     std::string name = fdp.ConsumeRandomLengthString(STR_LEN);
     DrawingPaintType type = static_cast<DrawingPaintType>(fdp.ConsumeIntegral<uint8_t>());
     auto geVisualEffect = std::make_shared<GEVisualEffect>(name, type);
- 
+
     std::string tag = fdp.ConsumeRandomLengthString(STR_LEN);
     Drawing::Matrix param;
     param.SetMatrix(fdp.ConsumeFloatingPoint<float>(),
@@ -110,28 +110,28 @@ void GEVisualEffectFuzzTestMatrixParam(FuzzedDataProvider& fdp)
                     fdp.ConsumeFloatingPoint<float>());
     geVisualEffect->SetParam(tag, param);
 }
- 
+
 void GEVisualEffectFuzzTestVectorParams(FuzzedDataProvider& fdp)
 {
     std::string name = fdp.ConsumeRandomLengthString(STR_LEN);
     DrawingPaintType type = static_cast<DrawingPaintType>(fdp.ConsumeIntegral<uint8_t>());
     auto geVisualEffect = std::make_shared<GEVisualEffect>(name, type);
- 
+
     std::string tag = fdp.ConsumeRandomLengthString(STR_LEN);
- 
+
     size_t pairCount = fdp.ConsumeIntegral<uint8_t>() % VEC_SIZE_MAX;
     std::vector<std::pair<float, float>> pairVec;
     for (size_t i = 0; i <= pairCount; i++) {
         pairVec.push_back({fdp.ConsumeFloatingPoint<float>(), fdp.ConsumeFloatingPoint<float>()});
     }
     geVisualEffect->SetParam(tag, pairVec);
- 
+
     std::vector<Vector2f> vec2f;
     for (size_t i = 0; i <= pairCount; i++) {
         vec2f.push_back(Vector2f(fdp.ConsumeFloatingPoint<float>(), fdp.ConsumeFloatingPoint<float>()));
     }
     geVisualEffect->SetParam(tag, vec2f);
- 
+
     std::vector<Vector4f> vec4f;
     for (size_t i = 0; i <= pairCount; i++) {
         vec4f.push_back(Vector4f(fdp.ConsumeFloatingPoint<float>(),
@@ -140,34 +140,34 @@ void GEVisualEffectFuzzTestVectorParams(FuzzedDataProvider& fdp)
                                  fdp.ConsumeFloatingPoint<float>()));
     }
     geVisualEffect->SetParam(tag, vec4f);
- 
+
     std::vector<float> floatVec;
     for (size_t i = 0; i <= pairCount; i++) {
         floatVec.push_back(fdp.ConsumeFloatingPoint<float>());
     }
     geVisualEffect->SetParam(tag, floatVec);
 }
- 
+
 void GEVisualEffectFuzzTestSharedPtrParams(FuzzedDataProvider& fdp)
 {
     std::string name = fdp.ConsumeRandomLengthString(STR_LEN);
     DrawingPaintType type = static_cast<DrawingPaintType>(fdp.ConsumeIntegral<uint8_t>());
     auto geVisualEffect = std::make_shared<GEVisualEffect>(name, type);
- 
+
     std::string tag = fdp.ConsumeRandomLengthString(STR_LEN);
- 
+
     std::shared_ptr<Drawing::Image> imageParam = std::make_shared<Drawing::Image>();
     geVisualEffect->SetParam(tag, imageParam);
 }
- 
+
 void GEVisualEffectFuzzTestRectParams(FuzzedDataProvider& fdp)
 {
     std::string name = fdp.ConsumeRandomLengthString(STR_LEN);
     DrawingPaintType type = static_cast<DrawingPaintType>(fdp.ConsumeIntegral<uint8_t>());
     auto geVisualEffect = std::make_shared<GEVisualEffect>(name, type);
- 
+
     std::string tag = fdp.ConsumeRandomLengthString(STR_LEN);
- 
+
     GERRect geRect;
     geRect.left_ = fdp.ConsumeFloatingPoint<float>();
     geRect.top_ = fdp.ConsumeFloatingPoint<float>();
@@ -178,7 +178,7 @@ void GEVisualEffectFuzzTestRectParams(FuzzedDataProvider& fdp)
                                      fdp.ConsumeFloatingPoint<float>());
     }
     geVisualEffect->SetParam(tag, geRect);
- 
+
     Drawing::RectF rectF;
     rectF.SetLeft(fdp.ConsumeFloatingPoint<float>());
     rectF.SetTop(fdp.ConsumeFloatingPoint<float>());
@@ -188,7 +188,7 @@ void GEVisualEffectFuzzTestRectParams(FuzzedDataProvider& fdp)
     rectF.SetBottom(rectF.GetTop() + height);
     geVisualEffect->SetParam(tag, rectF);
 }
- 
+
 void GEVisualEffectFuzzTestVectorColorParams(FuzzedDataProvider& fdp)
 {
     std::string name = fdp.ConsumeRandomLengthString(STR_LEN);
@@ -196,7 +196,7 @@ void GEVisualEffectFuzzTestVectorColorParams(FuzzedDataProvider& fdp)
     auto geVisualEffect = std::make_shared<GEVisualEffect>(name, type);
  
     std::string tag = fdp.ConsumeRandomLengthString(STR_LEN);
- 
+
     Vector3f vec3f(fdp.ConsumeFloatingPoint<float>(),
                    fdp.ConsumeFloatingPoint<float>(),
                    fdp.ConsumeFloatingPoint<float>());
@@ -207,7 +207,7 @@ void GEVisualEffectFuzzTestVectorColorParams(FuzzedDataProvider& fdp)
                    fdp.ConsumeFloatingPoint<float>(),
                    fdp.ConsumeFloatingPoint<float>());
     geVisualEffect->SetParam(tag, vec4f);
- 
+
     Drawing::Color4f color4f;
     color4f.redF_ = fdp.ConsumeFloatingPoint<float>();
     color4f.greenF_ = fdp.ConsumeFloatingPoint<float>();
@@ -215,15 +215,15 @@ void GEVisualEffectFuzzTestVectorColorParams(FuzzedDataProvider& fdp)
     color4f.alphaF_ = fdp.ConsumeFloatingPoint<float>();
     geVisualEffect->SetParam(tag, color4f);
 }
- 
+
 void GEVisualEffectFuzzTestPointArrayParam(FuzzedDataProvider& fdp)
 {
     std::string name = fdp.ConsumeRandomLengthString(STR_LEN);
     DrawingPaintType type = static_cast<DrawingPaintType>(fdp.ConsumeIntegral<uint8_t>());
     auto geVisualEffect = std::make_shared<GEVisualEffect>(name, type);
- 
+
     std::string tag = fdp.ConsumeRandomLengthString(STR_LEN);
- 
+
     std::array<Drawing::Point, FUZZER_POINT_NUM> pointArray;
     for (size_t i = 0; i < FUZZER_POINT_NUM; i++) {
         pointArray[i] = Drawing::Point(fdp.ConsumeFloatingPoint<float>(),
@@ -231,17 +231,17 @@ void GEVisualEffectFuzzTestPointArrayParam(FuzzedDataProvider& fdp)
     }
     geVisualEffect->SetParam(tag, pointArray);
 }
- 
+
 void GEVisualEffectFuzzTestGetInterfaces(FuzzedDataProvider& fdp)
 {
     std::string name = fdp.ConsumeRandomLengthString(STR_LEN);
     DrawingPaintType type = static_cast<DrawingPaintType>(fdp.ConsumeIntegral<uint8_t>());
     auto geVisualEffect = std::make_shared<GEVisualEffect>(name, type);
-  
+
     std::string res = geVisualEffect->GetName();
     auto impl = geVisualEffect->GetImpl();
 }
- 
+
 } // namespace Drawing
 } // namespace Rosen
 } // namespace OHOS
