e3a55bf30546cc1d537194c62b88842362ca385f
Starloser <18795137338@163.com>
2026-06-15 11:44:28 +0800

    “图像大小限制回退“
    Signed-off-by: Starloser <18795137338@163.com>

--- src/pipeline/ge_render.cpp
+++ src/pipeline/ge_render.cpp
@@ -27,9 +27,6 @@
 
 namespace OHOS {
 namespace GraphicsEffectEngine {
-namespace {
-constexpr int MAX_IMAGE_DIMENSION = 10000;
-} // namespace
 #define PROPERTY_MESA_BLUR_ALL_ENABLED "persist.sys.graphic.kawaseDisable"
 #ifdef GE_OHOS
 bool GERender::isMesablurAllEnable_ = (std::atoi(
@@ -51,11 +48,6 @@ void GERender::DrawImageEffect(Drawing::Canvas& canvas, Drawing::GEVisualEffectC
         LOGE("GERender::DrawImageRect image is null");
         return;
     }
-    auto imageInfo = image->GetImageInfo();
-    if (imageInfo.GetWidth() > MAX_IMAGE_DIMENSION || imageInfo.GetHeight() > MAX_IMAGE_DIMENSION) {
-        LOGE("GERender::DrawImageRect image dimension exceeds limit, max is %{public}d", MAX_IMAGE_DIMENSION);
-        return;
-    }
 
     auto resImage = ApplyImageEffect(canvas, veContainer, {image, src, dst}, sampling);
     if (!resImage) {
@@ -74,11 +66,6 @@ std::shared_ptr<Drawing::Image> GERender::ApplyImageEffect(Drawing::Canvas& canv
 {
     if (!context.image) {
         LOGE("GERender::ApplyImageEffect image is null");
-        return nullptr;
-    }
-    auto imageInfo = context.image->GetImageInfo();
-    if (imageInfo.GetWidth() > MAX_IMAGE_DIMENSION || imageInfo.GetHeight() > MAX_IMAGE_DIMENSION) {
-        LOGE("GERender::ApplyImageEffect image dimension exceeds limit, max is %{public}d", MAX_IMAGE_DIMENSION);
         return nullptr;
     }
     auto resImage = context.image;
